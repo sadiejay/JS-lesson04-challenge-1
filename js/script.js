111a8805d17f6bbe32eb77276bffa3f1b56a28b0
Sadie <19538219+sadiejay@users.noreply.github.com>
2021-10-02 14:39:56 -0500

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// console.log("Hi!");
+
+var mainTitle = document.querySelector(`h1`);
+
+// console.log(mainTitle);
+
+mainTitle.innerText = `Yay, Javascript!`;
+
+var body = document.querySelector(`body`);
+
+// console.log(body);
+
+body.style.backgroundColor = "#FEF2F1";
